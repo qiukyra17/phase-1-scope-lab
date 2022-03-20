163c4d9676a2001eb25405b24cbee155311a935f
kyra qiu <kyraqiu@gmail.com>
2022-03-19 23:07:34 -0400

    commit tree 0bf8ce49a992a2aa8184ca48bcd8f8602f790b2b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'could be bob';
+
+function upperCaseCustomerName() {customerName = 'BOB';}
+
+function setBestCustomer(){bestCustomer = 'not bob';}
+
+function overwriteBestCustomer(){bestCustomer = 'maybe bob';}
+
+function changeLeastFavoriteCustomer(){leastFavoriteCustomer = 'its bob';}
